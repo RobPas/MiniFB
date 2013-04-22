e299687d7a9a2042d267f052a62b49907e7e0365
Robert <madeincro@live.se>
2013-04-22 23:25:43 +0200

    MiniFB

---
+++ MiniFB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MiniFB
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

2d93f6e36f363708fca89ef0cb4ddbfc3dc1242b
Robert <madeincro@live.se>
2013-04-30 09:48:01 +0200

    user model

---
+++ MiniFB/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniFB.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public string Password { get; set; }
+    }
+}

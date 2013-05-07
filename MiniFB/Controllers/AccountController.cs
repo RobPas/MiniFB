675b80a493d3e286c1d8632a5b4c8e4377478977
Jesper Gustafsson <jesper256@gmail.com>
2013-05-07 15:19:18 +0200

    Test

--- MiniFB/Controllers/AccountController.cs
+++ MiniFB/Controllers/AccountController.cs
@@ -84,6 +84,8 @@ namespace MiniFB.Controllers
                     WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
                     WebSecurity.Login(model.UserName, model.Password);
 
+
+
                     return RedirectToAction("Index", "Home");
                 }
                 catch (MembershipCreateUserException e)
--- MiniFB/Models/Entities/User.cs
+++ MiniFB/Models/Entities/User.cs
@@ -30,7 +30,6 @@ namespace MiniFB.Models.Entities
         [DisplayName("Kön")]
         public string Sex { get; set; }
 
-
         public virtual List<NewsFeedItem> NewsFeedItems { get; set; }
 
         public int Age {

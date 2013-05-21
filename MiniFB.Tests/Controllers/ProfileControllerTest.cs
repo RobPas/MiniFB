eaf19d5986f3f244e4ae564b35dd4e53edd63767
Jesper Gustafsson <jesper256@gmail.com>
2013-05-21 12:35:17 +0200

    Testing response code

--- MiniFB.Tests/Controllers/ProfileControllerTest.cs
+++ MiniFB.Tests/Controllers/ProfileControllerTest.cs
@@ -8,20 +8,30 @@ using MiniFB.Controllers;
 using NUnit.Framework;
 using MiniFB.Models.Entities;
 using MiniFB.Models.Repositories;
+using System.Web;
+using System.IO;
 
 namespace MiniFB.Tests.Controllers
 {
     [TestFixture]
     public class ProfileControllerTest
     {
+        ProfileController controller;
+        Guid testGuid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testGuid = Guid.NewGuid();
+            var users = new List<User> { new User { UserName = "Test1", ID = testGuid, FirstName = "Test1" } };
+            var fakeUserRepo = new FakeRepository<User>(users.ToArray());
+            controller = new ProfileController(fakeUserRepo);
+        }
+
         [Test]
-        public void Index()
+        public void ProfileControllerTest_Index_returning_right_user_by_id()
         {
             // Arrange
-            var testGuid = Guid.NewGuid();
-            var users = new List<User> { new User { UserName = "Test1", ID = testGuid, FirstName = "Test1"}};
-            var fakeUserRepo = new FakeRepository<User>(users.ToArray());
-            ProfileController controller = new ProfileController(fakeUserRepo);
 
             // Act
             ViewResult result = controller.Index("Test1") as ViewResult;
@@ -30,5 +40,36 @@ namespace MiniFB.Tests.Controllers
             Assert.That((result.Model as User).ID , Is.EqualTo(testGuid));
         }
 
+
+        [Test]
+        public void ProfileControllerTest_Edit_returning_right_user_by_id()
+        {
+            // Arrange
+            
+
+            // Act
+            ViewResult result = controller.Index("Test1") as ViewResult;
+           
+            // Assert
+            Assert.That((result.Model as User).UserName, Is.EqualTo("Test1"));
+        }
+
+
+        [Test]
+        public void ProfileControllerTest_Edit_returning_ERROR_404_if_user_equals_to_null()
+        {
+            // Arrange
+            var response = new HttpResponse(TextWriter.Null);
+
+            // Act
+            ViewResult result = controller.Index("ThisUsernameDosntExist") as ViewResult;
+
+
+            
+            Console.Write(controller.Response.StatusCode);
+            // Assert
+            Assert.That(response.StatusCode, Is.EqualTo(404));
+            
+        }
     }
 }

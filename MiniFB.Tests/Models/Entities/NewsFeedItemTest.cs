1d2bad2aca143a9037b0620900400eda734761db
Carl Räfting <carl.rafting@gmail.com>
2013-05-22 14:13:46 +0200

    Nu funkar visning utav användarnamn i nyhetsflödet.

---
+++ MiniFB.Tests/Models/Entities/NewsFeedItemTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MiniFB.Tests.Models.Entities
+{
+    [TestFixture]
+    public class NewsFeedItemTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSomething() { }
+    }
+}

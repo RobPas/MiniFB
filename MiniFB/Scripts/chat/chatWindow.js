984446e804df168d9c630138d95c9d0db3c724ad
Pontus Wahlgren <poppe.wahl@gmail.com>
2013-05-23 14:41:59 +0200

    Små fix med chattrutan.

--- MiniFB/Scripts/chat/chatWindow.js
+++ MiniFB/Scripts/chat/chatWindow.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
     $(".chat").click(function () {
         $(".chat-window").slideToggle("slow");
+        $(".chat-content").click(function (event) {
+            event.stopPropagation();
+        });
     });
 });

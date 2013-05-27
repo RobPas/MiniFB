243e6364e6df1af1863aadbe394b967a4cc7c53c
Robert <madeincro@live.se>
2013-05-27 15:26:57 +0200

    Se tidigare commit...

---
+++ MiniFB/Models/Entities/FileStringResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniFB.Models.Entities
+{
+    public class FileStringResult : FileResult
+    {
+        public string Data { get; set; }
+
+        public FileStringResult(string data, string contentType)
+            : base(contentType)
+        {
+            Data = data;
+        }
+
+        protected override void WriteFile(HttpResponseBase response)
+        {
+            if (Data == null) { return; }
+            response.Write(Data);
+        }
+    }
+}

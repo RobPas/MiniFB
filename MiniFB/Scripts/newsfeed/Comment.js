bcd5886a29f16bc656b5f916a062c9d5ef86bf34
Jesper Gustafsson <jesper256@gmail.com>
2013-06-17 14:36:23 +0200

    Snyggade till och lade till så man kan dölja och visa komentrarer ifall det är fler än 5. snyggade till lite i koden m.m.

---
+++ MiniFB/Scripts/newsfeed/Comment.js
@@ -0,0 +1,59 @@
+﻿var ShowCommentForm = function (id) {
+    $("#comment-form-" + id).toggle();
+}
+
+var showAllComments = function (id) {
+
+    $("#comment-hidden-" + id).slideToggle('fast', function () {
+        // Animation complete.
+    });
+}
+
+var SendComment = function (writer, newsfeeditemguid) {
+
+    if ($("#commenttext-" + newsfeeditemguid).val() != "") {
+
+        var message = $("#commenttext-" + newsfeeditemguid).val();
+
+        $.ajax("/NewsFeedItem/AddComment/", {
+
+            type: 'POST',
+
+            data: { newsfeedid: newsfeeditemguid, commenttext: message },
+
+            success: function () {
+                $("#comments-" + newsfeeditemguid + " ul").first().next().append("<li><strong>" + writer + ":</strong> " + message + "</li>").fadeOut("fast").fadeIn("fast");
+                $("#commenttext-" + newsfeeditemguid).val("");
+            },
+            error: function () {
+
+            },
+            beforeSend: function () {
+
+            },
+            complete: function () {
+
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+
+
+    $(".submit-comment").click(function (e) {
+        e.preventDefault();
+        
+    });
+
+    $(".showCommentsButton").click(function (e) {
+        e.preventDefault();
+        $(this).text(function (i, text) {
+            return text === "Visa fler kommentarer" ? "Dölj kommentarer" : "Visa fler kommentarer";
+        })
+    });
+
+    
+
+
+});

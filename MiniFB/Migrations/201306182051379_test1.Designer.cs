268a93746bbb4e7973223e783817ebe221b29dd7
Jesper Gustafsson <jesper256@gmail.com>
2013-06-18 23:18:04 +0200

    test

--- MiniFB/Migrations/201306182051379_test1.Designer.cs
+++ MiniFB/Migrations/201306182051379_test1.Designer.cs
@@ -5,13 +5,13 @@ namespace MiniFB.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    public sealed partial class test : IMigrationMetadata
+    public sealed partial class test1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(test));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(test1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201306182040004_test"; }
+            get { return "201306182051379_test1"; }
         }
         
         string IMigrationMetadata.Source
